176d839053b04f5886d575076f2e4f00f0d4b8da
Ursula Miller <ursula.miller+fi@gmail.com>
2018-08-09 13:28:13 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+var katzDeliLine = ["Early Bird"]
+function takeANumber(line, name) {
+  var line = new Array();
+  line.push(name)
+  return line
+}
+
+console.log(line)
+takeANumber(katzDeliLine, "Ada"); // "Welcome, Ada. You are number 1 in line."
+takeANumber(katzDeliLine, "Grace"); // "Welcome, Grace. You are number 2 in line."
+takeANumber(katzDeliLine, "Kent");
