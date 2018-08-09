0f1b96b5f88940d835694270cde6c266fbab3059
Ursula Miller <ursula.miller+fi@gmail.com>
2018-08-09 14:56:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -45,13 +45,15 @@ function currentLine(line) {
   var i = line.length;
   var place = line.indexOf()
   var name = line[i]
-  // var counter = line.join(`, ` + line.indexOf(name) + ` `);
-  var counter
-  for ( place = 0; place < i ; place++) {
-    counter = counter + place + ". " + name;
-    console.log(counter)
-    return counter
-  }
+
+  // var counter
+  // for ( place = 0; place < i ; place++) {
+  //   counter = counter + place + ". " + name;
+  //   console.log(counter)
+  //   return counter
+  // }
+
+  var counter = line.join(`, ` + line.indexOf(name) + ` `);
   if (i>0) {
   var wholeMessage = `The line is currently: 1. ${counter}`;
   console.log(wholeMessage)
