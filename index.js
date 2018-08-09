b0f959b054a1a46c475e1d0d421e38d6210729ef
Ursula Miller <ursula.miller+fi@gmail.com>
2018-08-09 13:28:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ function takeANumber(line, name) {
   return line
 }
 
-console.log(line)
+
 takeANumber(katzDeliLine, "Ada"); // "Welcome, Ada. You are number 1 in line."
 takeANumber(katzDeliLine, "Grace"); // "Welcome, Grace. You are number 2 in line."
 takeANumber(katzDeliLine, "Kent");
+
+console.log(line)
