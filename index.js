6c1bff0be416b7b932cd51d12fdd0faaf2d3afe4
Ursula Miller <ursula.miller+fi@gmail.com>
2018-08-09 13:42:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,13 @@ function takeANumber(line, name) {
 takeANumber(katzDeliLine, "Ada"); // "Welcome, Ada. You are number 1 in line."
 takeANumber(katzDeliLine, "Grace"); // "Welcome, Grace. You are number 2 in line."
 takeANumber(katzDeliLine, "Kent");
+
+function nowServing(line) {
+  return line[0]
+  console.log(line[0])
+  line.shift
+  if (line[null]) {
+    message = `There is nobody waiting to be served`
+  }
+
+}
